0e168764eeee3b1554297400a9f0a4288a628fa4
barshakc <kcbarsha78@gmail.com>
2026-02-06 18:18:15 +0545

    Add auth test

---
+++ tracker/app/transactions/tests/test_auth.py
@@ -0,0 +1,24 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class AuthTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username ='testuser',
+            password ='testpassword'
+        )
+
+    def test_login_success(self):
+        response = self.client.post(
+            reverse('login'),
+            {'username': 'testuser', 'password': 'testpassword'},
+            format='json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
