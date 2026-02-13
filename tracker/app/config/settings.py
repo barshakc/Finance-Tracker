2c725ecfe997f341c814ce7513790680a80b46c8
barshakc <kcbarsha78@gmail.com>
2026-02-13 16:13:17 +0545

    Update settings

--- tracker/app/config/settings.py
+++ tracker/app/config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 AUTH_USER_MODEL = 'transactions.User'
 
 DEBUG = False
