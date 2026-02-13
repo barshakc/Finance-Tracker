4840b739e35c024b589e57b80def01699568e85a
barshakc <kcbarsha78@gmail.com>
2026-02-13 16:05:37 +0545

    Update test file

--- tracker/app/transactions/tests/test_transactions.py
+++ tracker/app/transactions/tests/test_transactions.py
@@ -1,6 +1,5 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
-from rest_framework import status
 from django.contrib.auth import get_user_model
 from transactions.models import Transaction, Category
 from django.utils import timezone
