e00aa1a3cb818f49e962cc9af1625bf6598cec11
barshakc <kcbarsha78@gmail.com>
2026-02-13 16:39:08 +0545

    Update settings file

--- tracker/app/config/settings.py
+++ tracker/app/config/settings.py
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-
-import os
-
 SECRET_KEY = os.getenv("SECRET_KEY")
 if not SECRET_KEY:
     raise ValueError("SECRET_KEY environment variable is not set!")
